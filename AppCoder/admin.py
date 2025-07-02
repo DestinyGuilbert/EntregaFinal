5f56cc99a0240a2bad078047abcffb8c9fd4425d
Destiny Ashley <guilbertashley84@gmail.com>
2025-07-02 17:44:17 -0600

    Finalizando

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -5,9 +5,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
 
-
-
-
 class UserAdmin(BaseUserAdmin):
 
     list_display = ("username", "email", "first_name", "last_name")
--- AppCoder/migrations/0001_initial.py
+++ AppCoder/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Curso',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nombre', models.CharField(max_length=40)),
+                ('nombre', models.CharField(max_length=20)),
                 ('camada', models.IntegerField()),
             ],
         ),
